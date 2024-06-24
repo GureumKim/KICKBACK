751a8302f77c8b7992f29e1c3f602444a40966ef
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-06-24 18:48:03 +0900

    Update firebase.js

--- WebFrontend/kickback/firebase.js
+++ WebFrontend/kickback/firebase.js
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBl3jCSzXOkm8N1SOO6FXNsNMFbvWy_jl4",
-  authDomain: "testset-58839.firebaseapp.com",
-  projectId: "testset-58839",
-  storageBucket: "testset-58839.appspot.com",
-  messagingSenderId: "784857668398",
-  appId: "1:784857668398:web:aa8db8a6322c341cc9b8bf",
-  measurementId: "G-96C2YQWNX8",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
